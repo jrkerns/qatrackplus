ad3ec25897ee9a088846c894eff1e1270aaa3f05
Wenze van Klink <w.vanklink@vumc.nl>
2014-06-10 09:28:11 +0200

    set up initial class based views

--- qatrack/tasks/views.py
+++ qatrack/tasks/views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+from django.views.generic import FormView, ListView
+
+
+class AddTask(FormView):
+    pass
+
+
+class EditTask(FormView):
+    pass
+
+
+class MyTasks(ListView):
+    pass
+
+
+class TaskOverview(ListView):
+    pass
