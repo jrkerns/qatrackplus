d2e9cc4f23e51e02b3b4de27b5c9154d533da3e2
Wenze van Klink <w.vanklink@vumc.nl>
2014-06-10 09:27:39 +0200

    add url to edit a task

--- qatrack/tasks/urls.py
+++ qatrack/tasks/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url
 
-from views import AddTask, MyTasks, TaskOverview
+from views import AddTask, MyTasks, TaskOverview, EditTask
 
 urlpatterns = patterns('',
     url(r'^add/$', AddTask.as_view(), name="tasks_add_task"),
+    url(r'^edit/(?P<pk>\d+)/$', EditTask.as_view(), name="tasks_edit_task"),
     url(r'^mytasks/$', MyTasks.as_view(), name="tasks_my_tasks"),
     url(r'^overview/$', TaskOverview.as_view(), name="tasks_overview"),
 )
