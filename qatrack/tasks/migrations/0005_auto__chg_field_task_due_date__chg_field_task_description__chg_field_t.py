019d5504d8a9de730f865dee29c97ebdc397c585
Wenze van Klink <w.vanklink@vumc.nl>
2014-06-24 12:58:09 +0200

    add migrations for model changes

---
+++ qatrack/tasks/migrations/0005_auto__chg_field_task_due_date__chg_field_task_description__chg_field_t.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Task.due_date'
+        db.alter_column('tasks_task', 'due_date', self.gf('django.db.models.fields.DateField')(null=True))
+
+        # Changing field 'Task.description'
+        db.alter_column('tasks_task', 'description', self.gf('django.db.models.fields.TextField')(null=True))
+
+        # Changing field 'Task.overdue_date'
+        db.alter_column('tasks_task', 'overdue_date', self.gf('django.db.models.fields.DateField')(null=True))
+
+    def backwards(self, orm):
+
+        # Changing field 'Task.due_date'
+        db.alter_column('tasks_task', 'due_date', self.gf('django.db.models.fields.DateField')(default=1))
+
+        # Changing field 'Task.description'
+        db.alter_column('tasks_task', 'description', self.gf('django.db.models.fields.TextField')(default=1))
+
+        # Changing field 'Task.overdue_date'
+        db.alter_column('tasks_task', 'overdue_date', self.gf('django.db.models.fields.DateField')(default=1))
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'tasks.category': {
+            'Meta': {'ordering': "('name',)", 'object_name': 'Category'},
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '255'})
+        },
+        'tasks.task': {
+            'Meta': {'ordering': "('-date_modified',)", 'object_name': 'Task'},
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tasks.Category']"}),
+            'comment_for_reviewer': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'date_modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'due_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'edit_groups': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'groups edit'", 'null': 'True', 'symmetrical': 'False', 'to': "orm['auth.Group']"}),
+            'edit_users': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'users edit'", 'null': 'True', 'symmetrical': 'False', 'to': "orm['auth.User']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'overdue_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'perform_groups': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'groups perform'", 'null': 'True', 'symmetrical': 'False', 'to': "orm['auth.Group']"}),
+            'perform_users': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'users perform'", 'null': 'True', 'symmetrical': 'False', 'to': "orm['auth.User']"}),
+            'status': ('django.db.models.fields.CharField', [], {'default': "'not_done'", 'max_length': '15'}),
+            'unit': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['units.Unit']", 'null': 'True', 'blank': 'True'})
+        },
+        'units.modality': {
+            'Meta': {'unique_together': "[('type', 'energy')]", 'object_name': 'Modality'},
+            'energy': ('django.db.models.fields.FloatField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '20'})
+        },
+        'units.unit': {
+            'Meta': {'ordering': "['number']", 'object_name': 'Unit'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'install_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'location': ('django.db.models.fields.CharField', [], {'max_length': '256', 'null': 'True', 'blank': 'True'}),
+            'modalities': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['units.Modality']", 'symmetrical': 'False'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'number': ('django.db.models.fields.PositiveIntegerField', [], {'unique': 'True'}),
+            'serial_number': ('django.db.models.fields.CharField', [], {'max_length': '256', 'null': 'True', 'blank': 'True'}),
+            'type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['units.UnitType']"})
+        },
+        'units.unittype': {
+            'Meta': {'unique_together': "[('name', 'model')]", 'object_name': 'UnitType'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'vendor': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['tasks']
