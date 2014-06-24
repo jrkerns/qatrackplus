44ee8dbf43cde0f3dafbc0eaed06d7e84079dba8
Wenze van Klink <w.vanklink@vumc.nl>
2014-06-24 12:54:07 +0200

    Tasks model: - status_choices: renamed the user viewable name - add Category to the model - renamed/edited some fields and descriptions - add model methods to check if a user can edit of perform a task - add model methods to check if a task is due or overdue

--- qatrack/tasks/models.py
+++ qatrack/tasks/models.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.db import models
 from django.contrib.auth.models import User, Group
 from django.utils.translation import ugettext as _
@@ -29,9 +31,9 @@ class Category(models.Model):
 
 
 class Task(models.Model):
-    NOT_DONE = "not_done"
-    DONE = "done"
-    REVIEWED = "reviewed"
+    NOT_DONE = "Not done"
+    DONE = "Done"
+    REVIEWED = "Reviewed"
 
     status_choices = (
         (NOT_DONE, _("Not done")),
@@ -40,19 +42,21 @@ class Task(models.Model):
     )
 
     name = models.CharField(verbose_name=_("Name"), max_length=100)
+    category = models.ForeignKey(Category, verbose_name=_("Category"))
     unit = models.ForeignKey(Unit, verbose_name=_("Unit"),
                              help_text=_("Assign this task to a unit for easy filtering"),
                              null=True, blank=True)
-    description = models.TextField(verbose_name=_("Description"), validators=[MinLengthValidator(100)],
-                                   help_text=_("Describe the meaning of the task and what should be done"))
-    due_date = models.DateField(verbose_name=_("Due date"), help_text=_("Set due date"))
-    overdue_date = models.DateField(verbose_name=_("Overdue date"), help_text=_("Set overdue date"))
+    description = models.TextField(verbose_name=_("Description"), validators=[MinLengthValidator(50)],
+                                   help_text=_("Describe the meaning of the task and what should be done"),
+                                   null=True, blank=True)
+    due_date = models.DateField(verbose_name=_("Due date"), help_text=_("Set due date"), null=True, blank=True)
+    overdue_date = models.DateField(verbose_name=_("Overdue date"), help_text=_("Set overdue date"), null=True, blank=True)
     comment_for_reviewer = models.TextField(verbose_name=_("Comment for reviewer"),
                                             help_text=_("Give a comment for the reviewer, describe what is done"),
                                             null=True, blank=True)
     edit_users = models.ManyToManyField(User, verbose_name=_("Users edit"), help_text=_("Users who can edit this task"),
                                         null=True, blank=True, related_name="users edit")
-    edit_status_groups = models.ManyToManyField(Group, verbose_name=_("Groups edit"),
+    edit_groups = models.ManyToManyField(Group, verbose_name=_("Groups edit"),
                                                 help_text=_("Groups who can edit this task"),
                                                 null=True, blank=True, related_name="groups edit")
     perform_users = models.ManyToManyField(User, verbose_name=_("User performs"),
@@ -61,8 +65,36 @@ class Task(models.Model):
     perform_groups = models.ManyToManyField(Group, verbose_name=_("Groups peform"),
                                             help_text=_("Groups who can perform this task"),
                                             null=True, blank=True, related_name="groups perform")
-    date_modified = models.DateTimeField(verbose_name=_("Date modified"), auto_now=True, editable=False)
-    status = models.CharField(verbose_name=_("Status"), max_length=15, choices=status_choices)
+    date_modified = models.DateTimeField(verbose_name=_("Date modified"), auto_now=True)
+    status = models.CharField(verbose_name=_("Status"), max_length=15, choices=status_choices, default="not_done")
 
     class Meta:
-        ordering = ("date_modified",)
+        ordering = ("-date_modified",)
+
+    def __unicode__(self):
+        """return display representation of object"""
+        return self.name
+
+    def can_edit(self, user):
+        if user in self.edit_users.all() or \
+                len(user.groups.filter(name__in=self.edit_groups.values_list('name', flat=True))) > 0:
+            return True
+        else:
+            return False
+
+    def can_perform(self, user):
+        if user in self.perform_users.all() or \
+                len(user.groups.filter(name__in=self.perform_groups.values_list('name', flat=True))) > 0:
+            return True
+        else:
+            return False
+
+    def is_past_due(self):
+        if date.today() > self.due_date:
+            return True
+        return False
+
+    def is_past_overdue(self):
+        if date.today() > self.overdue_date:
+            return True
+        return False
