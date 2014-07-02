627f0a2c972a3f80b290f678c423ac7b6d604e3d
Wenze van Klink <w.vanklink@vumc.nl>
2014-07-02 11:20:55 +0200

    removed datatableview app changed the URL names so they will work with the new view names

--- qatrack/tasks/urls.py
+++ qatrack/tasks/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, url
 
-from views import AddTask, MyTasks, TaskOverview, EditTask
+from views import AddTask, MyTasks, TaskOverview, EditPerformOrViewTask
 
 urlpatterns = patterns('',
     url(r'^add/$', AddTask.as_view(), name="tasks_add_task"),
-    url(r'^edit/(?P<pk>\d+)/$', EditTask.as_view(), name="tasks_edit_task"),
+    url(r'^edit/(?P<pk>\d+)/$', EditPerformOrViewTask.as_view(), name="tasks_edit_task"),
     url(r'^mytasks/$', MyTasks.as_view(), name="tasks_my_tasks"),
     url(r'^overview/$', TaskOverview.as_view(), name="tasks_overview"),
 )
